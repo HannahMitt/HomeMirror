424a5e14d7cc3bdbd9c4965a4f4b1e97cc76de43
Hannah Mittelstaedt <hannahmittel@gmail.com>
2015-08-23 00:01:06 -0400

    Added latest xkcd comic to mirror

--- app/src/main/java/com/morristaedt/mirror/MirrorActivity.java
+++ app/src/main/java/com/morristaedt/mirror/MirrorActivity.java
@@ -3,15 +3,32 @@ package com.morristaedt.mirror;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.morristaedt.mirror.modules.TimeModule;
+import com.morristaedt.mirror.modules.XKCDModule;
+import com.squareup.picasso.Picasso;
 
 public class MirrorActivity extends ActionBarActivity {
 
     private TextView mMainText;
+    private ImageView mXKCDImage;
+
+    private XKCDModule.XKCDListener mXKCDListener = new XKCDModule.XKCDListener() {
+        @Override
+        public void onNewXKCDToday(String url) {
+            if (TextUtils.isEmpty(url)) {
+                mXKCDImage.setVisibility(View.GONE);
+            } else {
+                Picasso.with(MirrorActivity.this).load(url).into(mXKCDImage);
+                mXKCDImage.setVisibility(View.VISIBLE);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +45,7 @@ public class MirrorActivity extends ActionBarActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         mMainText = (TextView) findViewById(R.id.main_text);
+        mXKCDImage = (ImageView) findViewById(R.id.xkcd_image);
 
         setViewState();
     }
@@ -40,5 +58,6 @@ public class MirrorActivity extends ActionBarActivity {
 
     private void setViewState() {
         mMainText.setText(TimeModule.getTimeOfDayWelcome(getResources()));
+        XKCDModule.getXKCDForToday(mXKCDListener);
     }
 }
